26ab19a17355839c8408514e8ab93d59e7a2557d
RyukShi <aure16300P@gmail.com>
2023-02-24 17:43:37 +0100

    security improvement (SupabaseAuth.vue)
    CSS updated.

--- src/components/SupabaseAuth.vue
+++ src/components/SupabaseAuth.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { supabase } from '../supabase'
 import { useUserStore } from '../stores/user-store'
 import { useRouter } from 'vue-router'
@@ -14,11 +14,54 @@ const lastName = ref('')
 
 const signUpMode = ref(false)
 
+const errorMessages = {
+  email: null,
+  password: null,
+  confirmEmail: null,
+  confirmPassword: null,
+}
+
 const userStore = useUserStore()
 const router = useRouter()
 
-const isValidEmail = computed(() => isEmail(email.value) && email.value === confirmEmail.value)
-const isValidPassword = computed(() => password.value === confirmPassword.value)
+const isSameEmail = computed(() => email.value === confirmEmail.value)
+const isSamePassword = computed(() => password.value === confirmPassword.value)
+const isValidEmail = computed(() => isEmail(email.value))
+const isValidPassword = computed(() => password.value.length >= 8)
+const isFormComplete = computed(() => {
+  let formFields = null
+  if (signUpMode.value) {
+    formFields = [
+      email.value, confirmEmail.value, password.value,
+      confirmPassword.value, firstName.value, lastName.value
+    ]
+  } else {
+    formFields = [ email.value, password.value ]
+  }
+  return formFields.every((field) => field !== '')
+})
+
+const isValidForm = computed(() => {
+  if (!isFormComplete.value || !isValidEmail.value || !isValidPassword.value) return false
+  else if (confirmEmail.value && !isSameEmail.value) return false
+  else if (confirmPassword.value && !isSamePassword.value) return false
+  else return true
+})
+
+watch([isValidEmail, isSameEmail, isSamePassword, isValidPassword], (values) => {
+  const [validEmail, sameEmail, samePassword, validPassword] = values
+  if (!validEmail) errorMessages.email = 'Invalid email'
+  else errorMessages.email = null
+
+  if (signUpMode.value && !sameEmail) errorMessages.confirmEmail = 'Emails do not match'
+  else errorMessages.confirmEmail = null
+
+  if (signUpMode.value && !samePassword) errorMessages.confirmPassword = 'Passwords do not match'
+  else errorMessages.confirmPassword = null
+
+  if (signUpMode.value && !validPassword) errorMessages.password = 'Password must be at least 8 characters'
+  else errorMessages.password = null
+})
 
 const submitedForm = async () => {
   if (signUpMode.value) {
@@ -51,8 +94,8 @@ const submitedForm = async () => {
       userStore.setUser(user)
       userStore.login = true
 
-      // redirect to home page
-      router.push('/')
+      // redirect to my-wallet page
+      router.push('/my-wallet')
 
     } catch (error) {
       console.log(error);
@@ -68,9 +111,11 @@ const submitedForm = async () => {
     <form class="auth-form" @submit.prevent="submitedForm">
       <div>
         <input type="email" v-model="email" required placeholder="Your e-mail" />
+        <p v-if="errorMessages.email" class="error-message">{{ errorMessages.email }}</p>
       </div>
       <div v-if="signUpMode">
         <input type="email" v-model="confirmEmail" required placeholder="Confirm your e-mail" />
+        <p v-if="errorMessages.confirmEmail" class="error-message">{{ errorMessages.confirmEmail }}</p>
       </div>
       <div class="flex gap-x-4" v-if="signUpMode">
         <input type="text" v-model="firstName" required placeholder="Your firstname" />
@@ -78,12 +123,14 @@ const submitedForm = async () => {
       </div>
       <div>
         <input type="password" v-model="password" required placeholder="Your password" />
+        <p v-if="errorMessages.password" class="error-message">{{ errorMessages.password }}</p>
       </div>
       <div v-if="signUpMode">
         <input type="password" v-model="confirmPassword" required placeholder="Confirm your password" />
+        <p v-if="errorMessages.confirmPassword" class="error-message">{{ errorMessages.confirmPassword }}</p>
       </div>
-      <div class="centered gap-x-4">
-        <button class="btn btn-sky" type="submit">{{ (signUpMode) ? 'Sign up' : 'Sign in' }}</button>
+      <div class="flex flex-col items-center gap-y-10">
+        <button :disabled="!isValidForm" class="btn btn-sky" type="submit">{{ (signUpMode) ? 'Sign up' : 'Sign in' }}</button>
         <button class="btn btn-sky" type="button" @click="signUpMode = !signUpMode">
           {{ (signUpMode) ? 'You have already an account ?' : 'Register now !' }}
         </button>
@@ -94,6 +141,10 @@ const submitedForm = async () => {
 
 <style scoped>
 .auth-form {
-  @apply my-10 p-10 bg-sky-900 rounded-lg flex flex-col gap-y-6;
+  @apply my-10 p-10 bg-sky-700 rounded-lg flex flex-col gap-y-6;
+}
+
+.error-message {
+  @apply text-sm mt-2 px-2 bg-rose-500 max-w-max rounded-lg;
 }
 </style>
