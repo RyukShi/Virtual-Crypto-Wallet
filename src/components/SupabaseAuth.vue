0d1bdcdcecd02355f4f730804d5fe8f3be87ba1e
RyukShi <aure16300P@gmail.com>
2024-01-25 15:37:22 +0100

    Quasar integrated

--- src/components/SupabaseAuth.vue
+++ src/components/SupabaseAuth.vue
@@ -2,6 +2,7 @@
 import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { useUserStore, type UserAuthenticationData } from '@/stores/user-store'
+import type { QForm } from 'quasar'
 
 const email = ref('')
 const confirmEmail = ref('')
@@ -11,6 +12,7 @@ const firstName = ref('')
 const lastName = ref('')
 
 const signUpMode = ref(false)
+const authForm = ref<QForm>()
 
 const userStore = useUserStore()
 const router = useRouter()
@@ -18,11 +20,12 @@ const router = useRouter()
 const toggleModeButtonLabel = computed(() => (signUpMode.value) ? 'You have already an account ?' : 'Register now !')
 const submitButtonLabel = computed(() => (signUpMode.value) ? 'Sign up' : 'Sign in')
 
-const submittedForm = async () => {
+const handleSubmit = async () => {
+  const isValidForm = await authForm.value?.validate()
+  if (!isValidForm) return
+
   const data: UserAuthenticationData = { password: password.value, email: email.value }
   if (signUpMode.value) {
-    data.email = email.value
-    data.password = password.value
     data.firstName = firstName.value
     data.lastName = lastName.value
   }
@@ -35,31 +38,27 @@ const submittedForm = async () => {
 
 <template>
   <div class="centered">
-    <!-- disabled reloading -->
-    <form class="auth-form" @submit.prevent="submittedForm">
-      <div>
-        <input type="email" v-model="email" required placeholder="Your e-mail" />
-      </div>
+    <q-form class="p-4" ref="authForm" @submit.prevent="handleSubmit">
+
+      <q-input v-model="email" type="email" clearable required placeholder="Your e-mail" />
+      <q-input v-if="signUpMode" v-model="confirmEmail" type="email" clearable required
+        placeholder="Confirm your e-mail" />
+
       <div v-if="signUpMode">
-        <input type="email" v-model="confirmEmail" required placeholder="Confirm your e-mail" />
+        <q-input v-model="firstName" clearable required placeholder="Your first name" />
+        <q-input v-model="lastName" clearable required placeholder="Your last name" />
       </div>
-      <div class="flex gap-x-4" v-if="signUpMode">
-        <input type="text" v-model="firstName" required placeholder="Your firstname" />
-        <input type="text" v-model="lastName" required placeholder="Your lastname" />
+
+      <q-input v-model="password" type="password" clearable required placeholder="Your password" />
+      <q-input v-if="signUpMode" v-model="confirmPassword" type="password" clearable required
+        placeholder="Confirm your password" />
+
+      <div class="mt-8">
+        <q-btn class="mr-4" :label="submitButtonLabel" type="submit" color="primary" />
+        <q-btn @click="signUpMode = !signUpMode" :label="toggleModeButtonLabel" type="button" color="primary" />
       </div>
-      <div>
-        <input type="password" v-model="password" required placeholder="Your password" />
-      </div>
-      <div v-if="signUpMode">
-        <input type="password" v-model="confirmPassword" required placeholder="Confirm your password" />
-      </div>
-      <div class="flex flex-col items-center gap-y-10">
-        <button class="btn btn-sky" type="submit">{{ submitButtonLabel }}</button>
-        <button class="btn btn-sky" type="button" @click="signUpMode = !signUpMode">
-          {{ toggleModeButtonLabel }}
-        </button>
-        <RouterLink v-if="!signUpMode" to="/account-recovery">Forgot password ?</RouterLink>
-      </div>
-    </form>
+
+      <RouterLink v-if="!signUpMode" to="/account-recovery">Forgot password ?</RouterLink>
+    </q-form>
   </div>
 </template>
