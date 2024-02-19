bfdce953ed0d7850e41c0b56daff7b4cfc0f1f27
RyukShi <aure16300P@gmail.com>
2024-02-19 18:47:11 +0100

    CustomTable component updated

--- src/components/CustomTable.vue
+++ src/components/CustomTable.vue
@@ -4,12 +4,13 @@ import { type Asset } from '@/stores/api-store'
 type CustomTableProps = {
   columns: any[];
   rows: Asset[];
+  title: string;
 }
 
 const props = defineProps<CustomTableProps>()
 </script>
 
 <template>
-  <q-table flat bordered title="Assets" color="amber" :rows="props.rows" :columns="props.columns"
+  <q-table flat bordered :title="props.title" color="amber" :rows="props.rows" :columns="props.columns"
     :pagination="{ rowsPerPage: 25 }" />
 </template>
